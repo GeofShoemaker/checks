1614e42198caec00428bd678b706e1293c8edc4c
GeofShoemaker <43453430+GeofShoemaker@users.noreply.github.com>
2018-10-16 16:12:54 -0500

    Update __init__.py

--- cs50/2017/x/library/check50/__init__.py
+++ cs50/2017/x/library/check50/__init__.py
@@ -3,24 +3,19 @@ from check50 import *
 
 class LibraryTester(Checks):
 
-    @check()
-    def exists(self):
-        """LibraryTester.java exists"""
-	self.require("LibraryTester.java")
-#	self.require("Book.java")
-#	self.require("Patron.java")
+    #@check()
+    #def exists(self):
+    #    """LibraryTester.java, Book.java, and Patron.java exists"""
+	#self.require("LibraryTester.java, Book.java, Patron.java")
+	#self.require("Book.java")
+	#self.require("Patron.java")
 	
-    @check("exists")
+    @check()
     def compiles(self):
         """LibraryTester.java compiles"""
-        self.spawn("javac LibraryTester.java").exit(0)
+        self.spawn("javac LibraryTester.java Book.java Patron.java").exit(0)
 
     @check("compiles")
     def test1(self):
         """Correctly Outputs"""
-        self.spawn("java LibraryTester").stdout("true\n", "true\n")).exit(0)
-        self.spawn("java LibraryTester").stdout("true\n", "true\n")).exit(0)
-        self.spawn("java LibraryTester").stdout("true\n", "true\n")).exit(0)
-#My version
-
-    
+        self.spawn("java LibraryTester").stdout("true\ntrue\ntrue\nfalse\n", "true\ntrue\ntrue\nfalse\n").exit(0)
